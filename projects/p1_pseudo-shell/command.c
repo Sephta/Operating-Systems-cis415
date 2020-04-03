d516894503ddb02438a2427087e0a3f54f12aedf
Sephta-Linux-MintVM <seth.tal255@gmail.com>
2020-04-02 17:19:08 -0700

    init commit for project 1, also commiting now incase I delete everything accidently with make...

---
+++ projects/p1_pseudo-shell/command.c
@@ -0,0 +1,12 @@
+/* Seth Tal
+ * 04.04.2020
+ * Psuedo-Shell: In this project we were tasked with creating a Pseudo-shell environment that
+ *               that executes on the command line.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "command.h"
+
+
---
+++ projects/p1_pseudo-shell/main.c
@@ -0,0 +1,23 @@
+/* Seth Tal
+ * 04.04.2020
+ * Psuedo-Shell: In this project we were tasked with creating a Pseudo-shell environment that
+ *               that executes on the command line.
+*/
+
+
+#include "command.c"
+
+
+void usage(int argc, char** argv) {
+    if (argc > 2) {
+        printf("\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char** argv) {
+
+    usage(argc, argv);
+
+    return 0;
+}
