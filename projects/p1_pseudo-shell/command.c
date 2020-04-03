30c2ab200719a140903fd3adc02f6fcd76b024bd
Sephta-Linux-MintVM <seth.tal255@gmail.com>
2020-04-02 20:14:45 -0700

    done working for today. 04.04.2020

--- projects/p1_pseudo-shell/command.c
+++ projects/p1_pseudo-shell/command.c
@@ -9,4 +9,18 @@
 #include <stdlib.h>
 #include "command.h"
 
+// void listDir() {}
 
+// void showCurrentDir() {}
+
+// void makeDir(char *dirName) {}
+
+// void changeDir(char *dirName) {}
+
+// void copyFile(char *sourcePath, char *destinationPath) {}
+
+// void moveFile(char *sourcePath, char *destinationPath) {}
+
+// void deleteFile(char *filename) {}
+
+// void displayFile(char *filename) {}
--- projects/p1_pseudo-shell/main.c
+++ projects/p1_pseudo-shell/main.c
@@ -5,19 +5,94 @@
 */
 
 
+#include <string.h>
 #include "command.c"
 
-
-void usage(int argc, char** argv) {
-    if (!(argc >= 2 && argc <= 3)) {
-        fprintf(stderr, "Usage: ./pseudo-shell <flag> <filename>\n");
-        exit(EXIT_FAILURE);
+/* IN: usage(int, char**), 
+ * This function catches usage errors when 
+ * running the program.
+   OUT: none
+*/
+int usage(int argc, char** argv) {
+    
+    if (argc == 1) {
+        return 0;
+    } else {
+        FILE* check_if_exists = fopen(argv[2], "r");
+        char* flag = argv[1];
+        char* cf = "-f";
+        if (flag == cf && check_if_exists != NULL) {
+            return 2;
+        } else if (flag != cf && check_if_exists != NULL) {
+            fprintf(stderr, "ERR: Invalid flag. ~ flag: %s\n", argv[1]);
+            fprintf(stderr, "Usage: ./pseudo-shell\trun program in interactive mode.\n");
+            fprintf(stderr, "\t-f <filename>\trun program in file mode.\n");
+            exit(EXIT_FAILURE);
+        } else if (flag != cf && check_if_exists == NULL) {
+            fprintf(stderr, "ERR: invalid flag and filename. ~ flag: %s\n", argv[1]);
+            fprintf(stderr, "Usage: ./pseudo-shell\trun program in interactive mode.\n");
+            fprintf(stderr, "\t-f <filename>\trun program in file mode.\n");
+            exit(EXIT_FAILURE);
+        }
+        fclose(check_if_exists);
     }
+
+    return 0;
+
+    // if (argc == 1) {
+    //     printf("Running Pseudo-shell environment...\n");
+    //     return 0;
+    // } else if (!(argc >= 2 && argc <= 3)) {
+    //     fprintf(stderr, "Usage: ./pseudo-shell\trun program in interactive mode.\n");
+    //     fprintf(stderr, "\t-f <filename>\trun program in file mode.\n");
+    //     exit(EXIT_FAILURE);
+    // } else {
+    //     FILE* check_if_exists = fopen(argv[2], "r");
+    //     if (argv[1] != "-f" && check_if_exists != NULL) {
+    //         fprintf(stderr, "ERR: Invalid flag. ~ flag: %s\n", argv[1]);
+    //         exit(EXIT_FAILURE);
+    //     } else if (argv[1] != "-f" && check_if_exists == NULL) {
+    //         fprintf(stderr, "ERR: invalid flag and filename. ~ flag: %s\n", argv[1]);
+    //         exit(EXIT_FAILURE);
+    //     }
+    //     fclose(check_if_exists);
+    // }
+
+    // return 0;
+}
+
+void malloc_buf(char** buf, size_t size) {
+    *buf = (char *)malloc(sizeof(char) * size);
 }
 
 int main(int argc, char** argv) {
 
-    usage(argc, argv);
+    int active_file = usage(argc, argv);
+
+    if (active_file) { // file mode
+        printf("file mode initializing...\n");
+    } else { // interactive mode
+        printf("interactive mode initializing...\n");
+        size_t size = 255;
+        char* input_buf[size];
+        // malloc_buf(&input_buf, size);
+        int num_chars = 0;
+        printf("here3\n");
+        while (strcmp((const char *)*input_buf, (const char *)"exit\n") != 0) {
+            printf("here4\n");
+            printf(">>> ");
+
+            num_chars = getline(&input_buf[0], &size, stdin);
+
+            printf("%s\n", *input_buf);
+
+            printf("\n");
+        }
+
+        // printf("here1\n");
+        // free(input_buf);
+        // printf("here2\n");
+    }
 
     return 0;
 }
