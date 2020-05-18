6e76438eec25a927c40541019978efc8d8a47eae
Sephta-Linux-MintVM <seth.tal255@gmail.com>
2020-05-18 10:34:20 -0700

    init commit for project 3

---
+++ projects/p3_quacker/quacker.c
@@ -0,0 +1,19 @@
+/*
+ * Seth Tal
+ * 05/18/2020
+ * Project 3 - Quacker...
+*/
+
+
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Main
+int main()
+{
+    printf("Hello World\n");
+
+    return 1;
+}
